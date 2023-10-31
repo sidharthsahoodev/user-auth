5a773c7cd8db19f32e9330236673236bfd2c88ed
Sidharth Sahoo <98236370+sidharthsahoodev@users.noreply.github.com>
2023-10-31 13:18:58 +0530

    Email & Password Validation, and Password Encryption

--- src/main/java/org/example/dto/RegisterUserDto.java
+++ src/main/java/org/example/dto/RegisterUserDto.java
@@ -1,7 +1,20 @@
 package org.example.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class RegisterUserDto {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters long")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")
     private String password;
 
     public String getEmail() {
--- src/main/java/org/example/service/UserServiceImpl.java
+++ src/main/java/org/example/service/UserServiceImpl.java
@@ -5,27 +5,71 @@ import org.example.repository.UserRepository;
 import org.example.response.Command;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.regex.Pattern;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public Command signUp(String email, String password) {
-        User existingUser = userRepository.findByEmail(email);
+//
+//        Command validationCommand = validateEmailAndPassword(email, password);
+//        if (!validationCommand.isSuccessful()) {
+//            return validationCommand;
+//        }
 
+
+        User existingUser = userRepository.findByEmail(email);
         if (existingUser != null) {
             return new Command("User already exists", false);
         }
 
         User user = new User();
         user.setEmail(email);
-        user.setPassword(password);
+    //    user.setPassword(password);
+
+        // Hash the password before saving it
+        String hashedPassword = passwordEncoder.encode(password);
+        user.setPassword(hashedPassword);
 
         userRepository.save(user);
 
         return new Command("User successfully registered", true);
     }
+
+    private Command validateEmailAndPassword(String email, String password) {
+        // Email regex pattern
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+
+        // Password regex pattern
+        // The following regex ensures that the password is at least 8 characters long,
+        // contains at least one digit, one lower case letter, one upper case letter,
+        // and one special character.
+        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        Pattern passwordPattern = Pattern.compile(passwordRegex);
+
+        if (!emailPattern.matcher(email).matches()) {
+            return new Command("Invalid email format", false);
+        }
+
+        if (!passwordPattern.matcher(password).matches()) {
+            return new Command("Password must be at least 8 characters long, "
+                    + "contain at least one digit, one lower case letter, "
+                    + "one upper case letter, and one special character", false);
+        }
+
+        return new Command("Validation successful", true);
+    }
+
+
 }
